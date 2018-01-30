5529e20d7ecaa83bd74985bfafb6b1aa61a24a14
yoojia.chen <yoojiachen@gmail.com>
2018-01-30 15:22:23 +0800

    更新App测试主题

--- app/src/main/java/com/parkingwang/version/app/MainActivity.java
+++ app/src/main/java/com/parkingwang/version/app/MainActivity.java
@@ -9,10 +9,9 @@ import android.view.View;
 import com.parkingwang.version.NextVersion;
 import com.parkingwang.version.Scheduler;
 import com.parkingwang.version.Version;
-import com.parkingwang.version.fir.FirIMUrlSource;
-import com.parkingwang.version.fir.FirIMVersionParser;
-import com.parkingwang.version.wave.WaveProgressDownloader;
-import com.parkingwang.version.wave.WaveVersionHandler;
+import com.parkingwang.version.check.CheckVersionDownloader;
+import com.parkingwang.version.check.CheckVersionHandler;
+import com.parkingwang.version.source.MockAssetSource;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -30,11 +29,19 @@ public class MainActivity extends AppCompatActivity {
                         .setDebugEnabled(true)
                         .setLocalVersion(Version.local(1, "1.0.0"))
                         .runOn(Scheduler.NewThread.create())
-                        .addSource(FirIMUrlSource.fromPackageName("com.parkingwang.pad3",
-                                "token-token"))
-                        .addVersionParser(FirIMVersionParser.create())
-                        .addVersionFoundHandler(WaveVersionHandler.create(context))
-                        .addApkDownloader(WaveProgressDownloader.create(context))
+                        // Fir更新源
+//                        .addSource(FirIMUrlSource.fromPackageName("pack.name", "token-token"))
+//                        .addVersionParser(FirIMVersionParser.create())
+                        // Mock更新源
+                        .addSource(new MockAssetSource())
+                        // Wave主题
+//                        .addVersionFoundHandler(WaveVersionHandler.create(context))
+//                        .addApkDownloader(WaveProgressDownloader.create(context))
+                        // Rocket主题
+//                        .addVersionFoundHandler(RocketVersionHandler.create(context))
+                        // Check主题
+                        .addVersionFoundHandler(CheckVersionHandler.create(context))
+                        .addApkDownloader(CheckVersionDownloader.create(context))
                         .check();
 
             }
